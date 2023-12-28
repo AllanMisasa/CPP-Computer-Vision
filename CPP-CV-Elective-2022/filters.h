cf4d217fe89c6c20e90898d66ee9929dbd254517
Allan Misasa Nielsen <Singulytics@gmail.com>
2023-12-28 06:15:51 +0100

    ...

--- CPP-CV-Elective-2022/filters.h
+++ CPP-CV-Elective-2022/filters.h
@@ -7,6 +7,43 @@
 using namespace cv;
 using namespace std;
 
+const int max_value_H = 360/2;
+const int max_value = 255;
+const String window_capture_name = "Video Capture";
+const String window_detection_name = "Object Detection";
+int low_H = 0, low_S = 0, low_V = 0;
+int high_H = max_value_H, high_S = max_value, high_V = max_value;
+static void on_low_H_thresh_trackbar(int, void *)
+{
+	low_H = min(high_H-1, low_H);
+	setTrackbarPos("Low H", window_detection_name, low_H);
+}
+static void on_high_H_thresh_trackbar(int, void *)
+{
+	high_H = max(high_H, low_H+1);
+	setTrackbarPos("High H", window_detection_name, high_H);
+}
+static void on_low_S_thresh_trackbar(int, void *)
+{
+	low_S = min(high_S-1, low_S);
+	setTrackbarPos("Low S", window_detection_name, low_S);
+}
+static void on_high_S_thresh_trackbar(int, void *)
+{
+	high_S = max(high_S, low_S+1);
+	setTrackbarPos("High S", window_detection_name, high_S);
+}
+static void on_low_V_thresh_trackbar(int, void *)
+{
+	low_V = min(high_V-1, low_V);
+	setTrackbarPos("Low V", window_detection_name, low_V);
+}
+static void on_high_V_thresh_trackbar(int, void *)
+{
+	high_V = max(high_V, low_V+1);
+	setTrackbarPos("High V", window_detection_name, high_V);
+}
+
 Mat RemoveColor(Mat src, int color)
 {
 	vector<Mat> BGRChannels;
@@ -25,6 +62,36 @@ Mat RemoveColor(Mat src, int color)
 	}
 	imshow("Stripped image", src);
 	return src;
+}
+
+void HSV_track_bars(Mat img) {
+
+	Mat img_HSV, frame_threshold;	// Create Mat objects for HSV image and thresholded image
+
+    namedWindow(window_capture_name);
+    namedWindow(window_detection_name);
+	
+    // Trackbars to set thresholds for HSV values
+    createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
+    createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
+    createTrackbar("Low S", window_detection_name, &low_S, max_value, on_low_S_thresh_trackbar);
+    createTrackbar("High S", window_detection_name, &high_S, max_value, on_high_S_thresh_trackbar);
+    createTrackbar("Low V", window_detection_name, &low_V, max_value, on_low_V_thresh_trackbar);
+    createTrackbar("High V", window_detection_name, &high_V, max_value, on_high_V_thresh_trackbar);
+
+	while (true) {
+
+		cvtColor(img, img_HSV, COLOR_BGR2HSV);
+		inRange(img_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+
+		imshow(window_capture_name, img);
+		imshow(window_detection_name, frame_threshold);
+		char key = (char) waitKey(30);
+		if (key == 'q' || key == 27)
+		{
+			break;
+		}
+	}
 }
 
 /*
--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
 #include "CPP-CV-Elective-2022/analysis.h"
+#include "CPP-CV-Elective-2022/filters.h"
 
 #include <iostream>
 
@@ -10,59 +12,94 @@ using namespace std; // Standard C++ namespace
 using namespace cv; // OpenCV namespace
 
 int main() {
-	// Set path to image
-	std::string image_path = samples::findFile("starry_night.jpg");
+	std::string image_path = samples::findFile("starry_night.jpg");  	// Set path to image
+	Mat img = imread(image_path, IMREAD_COLOR);  	// Read image into Mat object
 
-	// Read image into Mat object
-	Mat img = imread(image_path, IMREAD_COLOR); 
+	Mat img_HSV, frame_threshold;	// Create Mat objects for HSV image and thresholded image
 
-	// Check if image cannot be read. If not, return error.
-	if (img.empty())
+	if (img.empty())	// Check if image cannot be read. If not, return error.
 	{
-		std::cout << "Image cannot be loaded" << std::endl; // Print error message
+		std::cout << "Image cannot be loaded" << std::endl;
 		return -1; // Return error
 	}
-	
-	vector<Mat> bgr_planes; // Vector to store the 3 channels of the image
-	split(img, bgr_planes); // Split the image into 3 channels
 
-	int histSize = 256; // number of bins
-	float range[] = { 0, 256 }; // the upper boundary is exclusive
-	const float* histRange[] = {range}; // range of the histogram
-	bool uniform = true, accumulate = false; // uniform distribution of bins and no accumulation
-	
-	Mat b_hist, g_hist, r_hist; // Mat objects to store the histograms
-	calcHist(&bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, histRange, uniform, accumulate); // Calculate histogram for blue channel
-	calcHist(&bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, histRange, uniform, accumulate); // Calculate histogram for green channel
-	calcHist(&bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, histRange, uniform, accumulate); // Calculate histogram for red channel
-	
-	// Plot the histograms
-	int hist_w = 512; int hist_h = 400;
-	int bin_w = cvRound((double)hist_w / histSize);
-	
-	Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0)); // Create a black image to store the histograms
-	
-	// Normalize the histograms
-	normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-	normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-	normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
-	
-	// Draw the histograms
-	for (int i = 1; i < histSize; i++) {
-		line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(b_hist.at<float>(i - 1))),
-			Point(bin_w * (i), hist_h - cvRound(b_hist.at<float>(i))),
-			Scalar(255, 0, 0), 2, 8, 0);
-		line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(g_hist.at<float>(i - 1))),
-			Point(bin_w * (i), hist_h - cvRound(g_hist.at<float>(i))),
-			Scalar(0, 255, 0), 2, 8, 0);
-		line(histImage, Point(bin_w * (i - 1), hist_h - cvRound(r_hist.at<float>(i - 1))),
-			Point(bin_w * (i), hist_h - cvRound(r_hist.at<float>(i))),
-			Scalar(0, 0, 255), 2, 8, 0);
-	}
-	
-	// Display the histograms
-	//imshow("Histogram", histImage);
-	//waitKey(0);
-	//return 0;
-	Histogram_color(img);
+	HSV_track_bars(img);	
 }
+
+/*
+const int max_value_H = 360/2;
+const int max_value = 255;
+const String window_capture_name = "Video Capture";
+const String window_detection_name = "Object Detection";
+int low_H = 0, low_S = 0, low_V = 0;
+int high_H = max_value_H, high_S = max_value, high_V = max_value;
+static void on_low_H_thresh_trackbar(int, void *)
+{
+    low_H = min(high_H-1, low_H);
+    setTrackbarPos("Low H", window_detection_name, low_H);
+}
+static void on_high_H_thresh_trackbar(int, void *)
+{
+    high_H = max(high_H, low_H+1);
+    setTrackbarPos("High H", window_detection_name, high_H);
+}
+static void on_low_S_thresh_trackbar(int, void *)
+{
+    low_S = min(high_S-1, low_S);
+    setTrackbarPos("Low S", window_detection_name, low_S);
+}
+static void on_high_S_thresh_trackbar(int, void *)
+{
+    high_S = max(high_S, low_S+1);
+    setTrackbarPos("High S", window_detection_name, high_S);
+}
+static void on_low_V_thresh_trackbar(int, void *)
+{
+    low_V = min(high_V-1, low_V);
+    setTrackbarPos("Low V", window_detection_name, low_V);
+}
+static void on_high_V_thresh_trackbar(int, void *)
+{
+    high_V = max(high_V, low_V+1);
+    setTrackbarPos("High V", window_detection_name, high_V);
+}
+
+int main() {
+	std::string image_path = samples::findFile("starry_night.jpg");  	// Set path to image
+	Mat img = imread(image_path, IMREAD_COLOR);  	// Read image into Mat object
+
+	Mat img_HSV, frame_threshold;	// Create Mat objects for HSV image and thresholded image
+
+	if (img.empty())	// Check if image cannot be read. If not, return error.
+	{
+		std::cout << "Image cannot be loaded" << std::endl;
+		return -1; // Return error
+	}
+
+    namedWindow(window_capture_name);
+    namedWindow(window_detection_name);
+	
+    // Trackbars to set thresholds for HSV values
+    createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
+    createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
+    createTrackbar("Low S", window_detection_name, &low_S, max_value, on_low_S_thresh_trackbar);
+    createTrackbar("High S", window_detection_name, &high_S, max_value, on_high_S_thresh_trackbar);
+    createTrackbar("Low V", window_detection_name, &low_V, max_value, on_low_V_thresh_trackbar);
+    createTrackbar("High V", window_detection_name, &high_V, max_value, on_high_V_thresh_trackbar);
+
+	while (true) {
+
+	cvtColor(img, img_HSV, COLOR_BGR2HSV);
+	inRange(img_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+
+	imshow(window_capture_name, img);
+	imshow(window_detection_name, frame_threshold);
+        char key = (char) waitKey(30);
+	if (key == 'q' || key == 27)
+	{
+		break;
+	}
+	}
+	return 0;
+}
+*/
